7ef34af64d7ed4387dda9b5d8295fe28b0977729
Shreya Ghorui <shreyaghorui202@gmail.com>
2026-02-22 01:39:03 +0530

    add this

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,7 +2,8 @@ import jwt from "jsonwebtoken";
 import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
-  const { JWT_SECRET } = ENV;
+  const { JWT_SECRET, NODE_ENV } = ENV;
+
   if (!JWT_SECRET) {
     throw new Error("JWT_SECRET is not configured");
   }
@@ -12,14 +13,11 @@ export const generateToken = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
-    secure: ENV.NODE_ENV === "development" ? false : true,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: NODE_ENV === "production",  // required for HTTPS
+    sameSite: NODE_ENV === "production" ? "none" : "lax",
   });
 
   return token;
 };
-
-// http://localhost
-// https://dsmakmk.com
